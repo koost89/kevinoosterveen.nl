f4064a596d47668404c55f72288b956cd1641ff0
Kevin <kevin@Kevin.localdomain>
2021-08-22 02:14:50 +0200

    Initial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Homepage for Kevin Oosterveen's personal website.">
+        <title>Nope, nothing here yet</title>
+
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        <script src="{{ mix('js/app.js') }}" defer></script>
+    </head>
+    <body class="antialiased">
+        <!-- Nope, nothing here either. -->
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-900 sm:items-center py-4 sm:pt-0">
+            <h1 class="text-4xl font-bold text-white"> It is done when it is done. </h1>
+        </div>
+    </body>
+</html>
---
+++ webpack.mix.js
@@ -0,0 +1,29 @@
+const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.webpackConfig({
+    devServer: {
+        host: '0.0.0.0',
+        port: 8080,
+    }
+})
+
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/css/app.scss', 'public/css')
+    .options({
+        postCss: [ tailwindcss('./tailwind.config.js') ],
+    })
+
+if (mix.inProduction()) {
+    mix.version();
+}
