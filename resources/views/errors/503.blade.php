067b1248812dab4fec1fb74830296ab50f6eede7
Kevin Oosterveen <kevinoosterveen@gmail.com>
2021-08-22 15:20:56 +0200

    Add custom 503

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Homepage for Kevin Oosterveen's personal website.">
-        <title>🥳</title>
+        <title>Nope, nothing here yet</title>
 
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
@@ -13,7 +13,7 @@
     <body class="antialiased">
         <!-- Nope, nothing here either. -->
         <div class="relative flex items-top justify-center min-h-screen bg-gray-900 sm:items-center py-4 sm:pt-0">
-            <h1 class="text-4xl font-bold text-white"> Welcome 🥳 </h1>
+            <h1 class="text-4xl font-bold text-white"> It is done when it is done. </h1>
         </div>
     </body>
 </html>
