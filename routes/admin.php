7ef1bfd43a3d4fee56bcad10405f73a2f3922fd8
Kevin <kevinoosterveen@gmail.com>
2021-09-25 02:14:20 +0200

    mend

---
+++ routes/admin.php
@@ -0,0 +1,10 @@
+<?php
+
+//Route::get('/dashboard', DashboardController::class);
+
+//Route::resource('posts', PostController::class);
+
+//Route::resource('tags', TagController::class)
+
+//Route::resource('elastic', ElasticSearchController::class)
+//  ->only('index', 'show');
